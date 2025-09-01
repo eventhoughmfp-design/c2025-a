93e8da8358a2ffdc4d166c705a8fd6d6be2d82dd
eventhoughmfp-design <eventhoughmfp@gmail.com>
2025-09-01 17:52:12 +0800

    [modify the code of is_prime]

--- level1/p02_is_prime/main.cpp
+++ level1/p02_is_prime/main.cpp
@@ -5,7 +5,10 @@ int main() {
     scanf("%d",&n);
     bool p=true;
     for(int i=2;i*i<=n;i++) {
-        if (n%i==0) p=false;
+        if (n%i==0) {
+            p=false;
+            break;
+        }
     }
     if (p) printf("n is prime\n");
     else printf("n is not prime\n");
