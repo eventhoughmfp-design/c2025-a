c681aeea2bc8fa18820d409f434eb59aea33496d
eventhoughmfp-design <eventhoughmfp@gmail.com>
2025-09-02 21:46:37 +0800

    [complete encrypt_decrypt (Caesar cipher)]

---
+++ level1/p05_encrypt_decrypt/main.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+char ch;
+void CaesarCipher(vector<char>& s,int deltal){
+    deltal%=26;
+    for(int i=0;;i++){
+        if((ch=getchar())!='\n'){
+            if (ch>='a' && ch<='z') {
+                s.push_back((ch-'a'+deltal)%26+'a');
+            }
+            else {
+                s.push_back(ch);
+            }
+        }
+        else{
+            break;
+        }
+    }
+}
+void decode_CaesarCipher(vector<char>& s,int deltal) {
+    deltal%=26;
+    for (int i=0;i<s.size();i++) {
+        if (s[i]<'a'||s[i]>'z') {
+            continue;
+        }
+        else if (s[i]-deltal>='a'){
+            s[i]=s[i]-deltal;
+        }
+        else {
+            s[i]='z'-('a'-(s[i]-deltal-1));
+        }
+    }
+}
+int main() {
+    int deltal;
+    cout<<"select a number as the offset of the Caesar cipher:";
+    cin>>deltal;
+    if ((ch=getchar())=='\n') {
+        cout<<"please write down a string of letters and press Enter to get the ciphertext:";
+    }
+    vector<char> s;
+    CaesarCipher(s,deltal);
+    for (int i=0;i<s.size();i++) {
+        cout<<s[i];
+    }
+    cout<<endl<<"press Enter and you'll get the original text:";
+    if ((ch=getchar())=='\n') {
+        decode_CaesarCipher(s,deltal);
+        for (int i=0;i<s.size();i++) {
+            cout<<s[i];
+        }
+    }
+    return 0;
+}
